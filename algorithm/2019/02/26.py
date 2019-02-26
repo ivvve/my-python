259f44cd7941e8b9bb3a80fadd17b05ff1cb6864
devson <sonyc5720@gmail.com>
2019-02-26 23:54:07 +0900

    20190226

--- algorithm/2019/02/26.py
+++ algorithm/2019/02/26.py
@@ -14,3 +14,18 @@ def tickets(people):
             return "NO"
 
     return "YES"
+
+# 348597 => [7,9,5,8,4,3]
+
+def digitize(n):
+    reversed_list = []
+    while n > 0:
+        reversed_list.append(n % 10)
+        n //= 10
+
+    return reversed_list
+
+### better solution
+
+def digitize(n):
+    return map(int, str(n)[::-1])
